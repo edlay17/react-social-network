5bdabf6e9b52d9d5276bae52bc2933722a5ef5b5
unknown <edlayweb@gmail.com>
2020-10-28 12:54:33 +0300

    ComponentContainers

---
+++ src/Components/Content/UserProfile/UserInfo/delUserInfoContainer.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import s from "./UserInfo.module.css"
+import UserInfoItem from "./UserInfoItem/UserInfoItem";
+import UserInfo from "./UserInfo";
+
+function UserInfoContainer(props) {
+    let infoData = props.store.getState().profile.profileInfoData;
+    let infoDataConvert = infoData.map((description) => <UserInfoItem infoItemName={description.descItemName} infoItem={description.descItem}/>);
+
+    return (
+        <UserInfo infoDataConvert={infoDataConvert}
+                  profileName={props.store.getState().profile.profileName}
+                  profileAvatar={props.store.getState().profile.profileAvatar}
+        />
+    );
+}
+
+export default UserInfoContainer;
+
