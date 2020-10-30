393ccdd15e7dddd61e2004871132029813a75a21
unknown <edlayweb@gmail.com>
2020-10-30 13:15:20 +0300

    fix addpost react-redux

---
+++ src/Components/Content/UserProfile/UserPosts/UserPostsContainer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {postTextareaChangeActionCreator, addPostActionCreator} from "../../../../../redux/profileReducer";
+import UserAddPost from "./UserAddPost";
+import {connect} from "react-redux";
+
+const mapStateToProps = (state) => {
+    return{
+        postData: state.profile.postData,
+        newPostText: state.profile.newPostText
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return{
+        addPost: () => {
+            dispatch(addPostActionCreator());
+            },
+        textareaChange: (text) => {
+            dispatch(postTextareaChangeActionCreator(text));
+        }
+    }
+}
+
+const UserAddPostContainer = connect(mapStateToProps, mapDispatchToProps)(UserAddPost);
+export default UserAddPostContainer;
+
